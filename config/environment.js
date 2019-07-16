ead020d1add10a7c9d085c2bc8e1040f1a6b85a4
ebenKB <eakbo23@gmail.com>
2019-07-16 16:31:49 +0000

    add api host for production

--- config/environment.js
+++ config/environment.js
@@ -49,6 +49,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.host = 'https://hubkbsblog.herokuapp.com/';
     // here you can enable a production-specific feature
   }
 
