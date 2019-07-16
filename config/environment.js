be936dfcf52c298395b2187a45e3ae83a0841fdc
ebenKB <eakbo23@gmail.com>
2019-07-16 16:37:16 +0000

    add api namespace for production

--- config/environment.js
+++ config/environment.js
@@ -50,6 +50,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.host = 'https://hubkbsblog.herokuapp.com/';
+    ENV.namespace = 'api/v1';
     // here you can enable a production-specific feature
   }
 
