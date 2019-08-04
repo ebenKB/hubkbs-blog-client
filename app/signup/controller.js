bae5ab65b90464ab2b37eed8ca653b85b5f58d55
ebenKB <eakbo23@gmail.com>
2019-08-04 20:54:52 +0000

    add load animation

--- app/signup/controller.js
+++ app/signup/controller.js
@@ -1,6 +1,6 @@
 import Controller from '@ember/controller';
 import UserValidations from 'hubkbs-blog/validations/user';
-import { get } from '@ember/object';
+import { get, set } from '@ember/object';
 
 export default Controller.extend({
   UserValidations,
@@ -9,25 +9,19 @@ export default Controller.extend({
   email: '',
   contact: '',
   password: '',
+  isAuthenticating: false,
 
   actions: {
     signup(changeset) {
-      // const user = {
-      //   firstname: this.firstname,
-      //   lastname: this.lastname,
-      //   email: this.email,
-      //   contact: this.contact,
-      //   password: this.password,
-      // };
-      // const created = this.store.createRecord('user', user);
-      // created.save();
+      set(this, 'isAuthenticating', true);
       if (this._checkPassword(get(changeset, 'password'),
         get(changeset, 'password_confirmation'))) {
         if (get(changeset, 'isValid')) {
-          changeset.save();
+          changeset.save()
+            .catch(() => set(this, 'isAuthenticating', false));
         }
       } else {
-        console.log('passwords do not match');
+        set(this, 'isAuthenticating', false);
       }
     }
   },
