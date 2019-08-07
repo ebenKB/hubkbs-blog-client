0dea9d9a9299764467f431082f51c7dbc52f9174
ebenKB <eakbo23@gmail.com>
2019-08-07 04:25:33 +0000

    update host url

--- app/dashboard/route.js
+++ app/dashboard/route.js
@@ -1,5 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service} from '@ember/service';
 
 export default Route.extend({
-
+  session: service(),
+  currentUser: service('current-user'),
+  beforeModel() {
+    if (this.currentUser.user) {
+      if (!this.currentUser.user.isAdmin) {
+        console.log('you are not an admin');
+        this.transitionTo('/');
+      }
+    } else this.transitionTo('/');
+  }
 });
